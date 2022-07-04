f6b0dba5a1cbe6eb3b7e943cbe01cfefb194b2f3
abmint <108632657+abmint@users.noreply.github.com>
2022-07-05 04:12:00 +0900

    Update wireguard.py

--- wireguard.py
+++ wireguard.py
@@ -1,7 +1,6 @@
-"""/*! SPDX-License-Identifier: GPL-2.0
- *
- * Copyright (C) 2015-2020 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
- */
+"""
+SPDX-License-Identifier: GPL-2.0
+Copyright (C) 2015-2020 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
 """
 
 import math
